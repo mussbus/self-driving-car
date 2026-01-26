4b776d2ddd393a9c0f631f49e3b4987c1ed9f1b2
Chad <chadmusser98@gmail.com>
2026-01-26 14:56:01 -0600

    added car class and controls class

---
+++ src/car.ts
@@ -0,0 +1,58 @@
+import Controls from "./controls";
+
+export default class Car {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  color: string;
+  controls: Controls;
+  speed: number;
+  max_speed: number;
+  acceleration: number;
+  friction: number;
+  direction: number;
+
+  constructor(x: number, y: number, width: number, height: number) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = "red";
+    this.controls = new Controls();
+    this.speed = 0;
+    this.max_speed = 10;
+    this.acceleration = 0.2;
+    this.friction = 0.03;
+    this.direction = 0;
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.direction);
+    ctx.fillStyle = this.color;
+    ctx.fillRect(-this.width / 2, -this.height / 2, this.width, this.height);
+  }
+
+  update() {
+    if (this.controls.forward) this.speed += this.acceleration;
+    if (this.controls.reverse) this.speed -= this.acceleration;
+    var change: number = 0;
+    if (this.controls.left) change = 0.03;
+    if (this.controls.right) change = -0.03;
+    if (this.speed < 0) change *= -1;
+    this.direction += change;
+    // this.direction = this.direction % Math.PI;
+    if (Math.abs(this.direction) < 0.03) this.direction = 0;
+
+    if (this.speed > this.max_speed) this.speed = this.max_speed;
+    if (this.speed < -this.max_speed / 2) this.speed = -this.max_speed / 2;
+    if (this.speed > 0) this.speed -= this.friction;
+    if (this.speed < 0) this.speed += this.friction;
+    if (Math.abs(this.speed) < 0.025) this.speed = 0;
+
+    this.x -= Math.sin(this.direction) * this.speed;
+    this.y -= Math.cos(this.direction) * this.speed;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,21 @@
+import Car from "./car";
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d")!;
-var height = 494;
-var height2 = 534;
-var height3 = 500;
 
-const move = true;
+const car = new Car(400, 500, 20, 40);
 
 function loop() {
+  canvas.height = window.innerHeight;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillStyle = "white";
-  ctx.fillRect(190, height3, 20, 40);
-  ctx.fillStyle = "black";
-  ctx.fillRect(186, height, 8, 12);
-  ctx.fillRect(206, height, 8, 12);
-  ctx.fillRect(186, height2, 8, 12);
-  ctx.fillRect(206, height2, 8, 12);
-
-  if (move) {
-    height--;
-    height2--;
-    height3--;
-  }
+  car.update();
+  car.draw(ctx);
 
   requestAnimationFrame(loop);
 }
 
 loop();
+
+// if (import.meta.hot) {
+//   import.meta.hot.accept();
+// }
